81ea0e631cad667b4e85d50b383ccc123fbbf29d
Goddo-ro <r0manovic1@yandex.ru>
2022-07-28 16:14:02 +0300

    first commit

---
+++ src/components/Starting/Starting.jsx
@@ -0,0 +1,12 @@
+import "./Starting.css"
+
+
+export default function Starting() {
+    return (
+        <div className="starting">
+            <h1 className="starting--h1">Quizzical</h1>
+            <h5 className="starting--h5">Some description if needed</h5>
+            <button class="btn start-btn">Start quiz</button>
+        </div>
+    )
+}
